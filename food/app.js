0517ecf4a8be858b7c15cb1c489ca5b1c1b8e45b
sengdee1111 <624608020007@dpu.ac.th>
2020-09-25 15:26:50 +0700

    food2

--- food/app.js
+++ food/app.js
@@ -2,16 +2,27 @@ const express = require('express');
 const app = express();
 
 const restaurantsRouter = require('./routes/restaurants');
+const path = require('path');
+const indexRouter = require('./routes/index');
 
 
 const port = process.env.PORT || 8000;
 
+const logger = require('./middleware/logger');
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname, 'public')));
+// Custom Middleware
+app.use(logger);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 // Routes
 app.use('/api/restaurants',restaurantsRouter);
+app.use('/',indexRouter);
 
 app.listen(port,'127.0.0.1',()=>{
 console.log(`Listening to request on port ${port}`);
